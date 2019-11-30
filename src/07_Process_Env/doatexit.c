f2003134e54223d81999854c953c4b5820dc0645
r00tk1ts <whereisr00tk1t@gmail.com>
2019-11-30 15:15:10 +0800

    update

--- src/07_Process_Env/doatexit.c
+++ src/07_Process_Env/doatexit.c
@@ -13,16 +13,17 @@ static void	my_exit2(void);
 
 int main(void)
 {
+    if (atexit(my_exit1) != 0)
+        err_sys("can't register my_exit1");
+
 	if (atexit(my_exit2) != 0)
 		err_sys("can't register my_exit2");
 
 	if (atexit(my_exit1) != 0)
 		err_sys("can't register my_exit1");
-	if (atexit(my_exit1) != 0)
-		err_sys("can't register my_exit1");
 
 	printf("main is done\n");
-	return(0);
+	return 0;
 }
 
 static void my_exit1(void)
--- src/07_Process_Env/echoarg.c
+++ src/07_Process_Env/echoarg.c
@@ -8,11 +8,18 @@
  */
 #include "apue.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *env[])
 {
 	int	i;
 
 	for (i = 0; i < argc; i++)		/* echo all command-line args */
 		printf("argv[%d]: %s\n", i, argv[i]);
+
+    for (i=0; ;++i)
+    {
+        if(env[i] == NULL)
+            break;
+        printf("env[%d]:%s\n", i, env[i]);
+    }
 	exit(0);
 }
